79905ef6a47a1d2b52dd01c4e1251c4c5f904bb7
oumar <23033@supnum.mr>
2025-11-25 12:27:08 +0000

    soap

--- src/main/java/com/example/TDSOA/soap/generated/package-info.java
+++ src/main/java/com/example/TDSOA/soap/generated/package-info.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par Eclipse Implementation of JAXB, v3.0.0 
 // Voir https://eclipse-ee4j.github.io/jaxb-ri 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2025.11.25 à 12:03:15 PM UTC 
+// Généré le : 2025.11.25 à 12:26:00 PM UTC 
 //
 
 @jakarta.xml.bind.annotation.XmlSchema(namespace = "http://example.com/servers", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
